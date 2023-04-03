5ce687f8b1e9abd1c51078b70b5ad1c11d8f1795
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-03 18:17:54 -0400

    removed unsed imports

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { Row, Col} from 'react-bootstrap'
 import { Canvas } from '@react-three/fiber'
 import '../assets/css/Home.css'
 import '../assets/css/Animations.css'
-// import '../js/Home.js'
 import Earth from '../components/Earth';
 import Login from '../components/Login'
 
