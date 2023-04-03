3a99fb20b590e30b21d3d93c72b0e053ab08d41b
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-03 17:34:18 -0400

    inserted all columns and rows

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,7 +14,27 @@ const Home = () => {
   return (
     <Row>
     <Col > 
-
+        <div className='container3D'>
+        <div class="flier">
+            <img id='image1' src='/assets/images/pizza.png' alt='floating pizza' />
+            <img id='image2' src='/assets/images/turkey.png' alt='floating turkey' />
+            <img id='image3' src='/assets/images/sushi.png' alt='floating sushi' />
+            <img id='image4' src='/assets/images/ice-cream.png' alt='floating ice cream' />
+            <img id='image5' src='/assets/images/cake.png' alt='floating cake' />
+            <img id='image6' src='/assets/images/chinese-food.png' alt='floating chinese takeout' />
+            <img id='image7' src='/assets/images/fried-chicken.png' alt='floating fried chicken' />
+            <img id='image8' src='/assets/images/fries.png' alt='floating fries' />
+        </div>
+            <Row className='middle'>
+      
+                <Col className="col-lg-5 col-md-7 p-0 form-container" style={{width: '25%', position: 'relative', zIndex: '1'}}>
+                </Col>
+                <Col xs lg="5" style={{ height: '100vh', padding: 0}} >
+             
+                </Col>
+            </Row>
+            
+        </div>
     </Col> 
 </Row>
   )
