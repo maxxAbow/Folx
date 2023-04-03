9a12757c614597cde7a50ca6d6102fa3d06599d6
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-03 18:01:00 -0400

    imported 3d model in public/assets/polyImages, loaded image via useLoader hook

--- client/src/components/Earth.jsx
+++ client/src/components/Earth.jsx
@@ -3,3 +3,11 @@ import { useFrame, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 // import '../styles/Home.css'
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
+
+function Earth() {
+
+    const gltf = useLoader(GLTFLoader, '/assets/polyImages/earth/scene.gltf')
+
+}
+
+export default Earth
