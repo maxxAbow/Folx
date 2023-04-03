6679d807f41e5d3df9add0b94d6ae8c5530acd8d
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-03 16:20:19 -0400

    imported all components

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,11 @@
-import React from 'react';
+import React, {Suspense, useState} from 'react'
+import { Row, Col} from 'react-bootstrap'
+import { Canvas } from '@react-three/fiber'
+import '../styles/Home.css'
+import '../styles/Animations.css'
+// import '../js/Home.js'
+import Earth from '../components/Earth';
+import Login from '../components/Login'
 
 const Home = () => {
   return <div>Home</div>;
