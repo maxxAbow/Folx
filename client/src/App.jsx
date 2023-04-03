aa70d6a89d34df67998e6ba19beb6f37b4932b89
David Mackey <davidmackey92@gmail.com>
2023-04-03 06:51:03 -0500

    ...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Navigation from './components/Navigation';
+
 function App() {
   return (
     <div className='App'>
-      <h1>App</h1>
+      <Navigation />
     </div>
   );
 }
