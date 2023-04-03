e5a456ebb603ded837b714288383c9837ac5e365
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-03 17:58:28 -0400

    imported all hooks and components for 3d image

---
+++ client/src/components/Earth.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect, useRef } from 'react'
+import { useFrame, useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+// import '../styles/Home.css'
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
