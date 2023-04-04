d3bba0057d8e59692239fc0d4158b189913d2588
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-03 21:44:41 -0400

    created hideImg function

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -10,23 +10,39 @@ const Home = () => {
 
   const [login, setLogin] = useState(true);
 
+// Randomly generates a number that represents an index in the array returned by the images
+  const hideImg = () => {
+    const imgArray = document.querySelectorAll('.space-food');
+
+    // If the array's length is 0, it will not continue the function
+    if (imgArray.length === 0) {
+        return
+    };
+    
+    // Randomly generates a number that's between 0 and the length of the array
+    let i = Math.floor(Math.random() * imgArray.length);
+
+    //uses the random number to remove an element from the DOM based of the index.
+    imgArray[i].remove()
+}
+
   return (
     <Row>
     <Col > 
         <div className='container3D'>
         <div class="flier">
-            <img id='image1' src='/assets/images/pizza.png' alt='floating pizza' />
-            <img id='image2' src='/assets/images/turkey.png' alt='floating turkey' />
-            <img id='image3' src='/assets/images/sushi.png' alt='floating sushi' />
-            <img id='image4' src='/assets/images/ice-cream.png' alt='floating ice cream' />
-            <img id='image5' src='/assets/images/cake.png' alt='floating cake' />
-            <img id='image6' src='/assets/images/chinese-food.png' alt='floating chinese takeout' />
-            <img id='image7' src='/assets/images/fried-chicken.png' alt='floating fried chicken' />
-            <img id='image8' src='/assets/images/fries.png' alt='floating fries' />
+            <img className="space-food" id='image1' src='/assets/images/pizza.png' alt='floating pizza' />
+            <img className="space-food" id='image2' src='/assets/images/turkey.png' alt='floating turkey' />
+            <img className="space-food" id='image3' src='/assets/images/sushi.png' alt='floating sushi' />
+            <img className="space-food" id='image4' src='/assets/images/ice-cream.png' alt='floating ice cream' />
+            <img className="space-food" id='image5' src='/assets/images/cake.png' alt='floating cake' />
+            <img className="space-food" id='image6' src='/assets/images/chinese-food.png' alt='floating chinese takeout' />
+            <img className="space-food" id='image7' src='/assets/images/fried-chicken.png' alt='floating fried chicken' />
+            <img className="space-food" id='image8' src='/assets/images/fries.png' alt='floating fries' />
         </div>
             <Row className='middle'>
                 <Col>
-                    <img className='astro bounce-2' src='/assets/images/astronaut-flipped.png' alt='A bouncing astronaut' />
+                    <img className='astro bounce-2' src='/assets/images/astronaut-flipped.png' alt='A bouncing astronaut' onClick={() => hideImg()}/>
                 </Col>
                 <Col className="col-lg-5 col-md-7 p-0 form-container" style={{width: '25%', position: 'relative', zIndex: '1'}}>
                     <Login login={login} setLogin={setLogin} />
