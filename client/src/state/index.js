349e205ea0e2e9d46e4d9ae296575d19459b4d51
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-05 01:05:55 -0400

    created initialState object

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -2,5 +2,10 @@ const { createSlice } = require("@reduxjs/toolkit");
 
 // The state that will be stored in our global state
 const initialState = {
-
+    //Represents Dark/Light mode state
+    mode: "light",
+    user: null,
+    //Represents all Auth info we will store
+    token: null,
+    post: []
 }
