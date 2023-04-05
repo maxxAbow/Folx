83fa3b9a3671d1d09ca9662de78075a99b683d3c
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-05 13:24:37 -0400

    Set state to determine to open up mobile menu on smaller/mobile screens

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -33,33 +33,40 @@ import { useNavigate } from 'react-router-dom';
 import FlexBetween from './style-components/FlexBetween';
 
 const Navigation = () => {
+  // State to determine to open up mobile menu on smaller/mobile screens
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
   return (
-    <Navbar collapseOnSelect expand='lg' bg='black' variant='dark' id='navbar'>
-      <Container>
-        <Navbar.Brand href='#home'>
-          FolX (But it'll be a logo here haha!)
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-        {/* <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse> */}
-      </Container>
-    </Navbar>
-  );
-};
+    <div></div>
+  )
+}
+// const Navigation = () => {
+//   return (
+//     <Navbar collapseOnSelect expand='lg' bg='black' variant='dark' id='navbar'>
+//       <Container>
+//         <Navbar.Brand href='#home'>
+//           FolX (But it'll be a logo here haha!)
+//         </Navbar.Brand>
+//         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+//         {/* <Navbar.Collapse id="responsive-navbar-nav">
+//           <Nav className="me-auto">
+//             <Nav.Link href="#features">Features</Nav.Link>
+//             <Nav.Link href="#pricing">Pricing</Nav.Link>
+//             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+//               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+//               <NavDropdown.Item href="#action/3.2">
+//                 Another action
+//               </NavDropdown.Item>
+//               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+//               <NavDropdown.Divider />
+//               <NavDropdown.Item href="#action/3.4">
+//                 Separated link
+//               </NavDropdown.Item>
+//             </NavDropdown>
+//           </Nav>
+//         </Navbar.Collapse> */}
+//       </Container>
+//     </Navbar>
+//   );
+// };
 
 export default Navigation;
