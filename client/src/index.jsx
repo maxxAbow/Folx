a06cf6c5e256608cac03d04932376b4e0d08b7b8
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-05 02:05:09 -0400

    configured state management

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -36,8 +36,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-
-    <App />
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
