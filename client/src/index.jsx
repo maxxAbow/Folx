7da6c1cd8c263c53078d4048b9fe9a458fdaaa51
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-05 02:04:05 -0400

    configured state management

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,12 +4,12 @@ import './assets/css/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import authReducer from "./state";
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import {
   persistStore,
   persistReducer,
-  Flush,
+  FLUSH,
   REHYDRATE,
   PAUSE,
   PERSIST,
@@ -19,9 +19,25 @@ import {
 import storage from "redux-persist/lib/storage"
 import { PersistGate } from 'redux-persist/integration/react';
 
+const persistConfig = {key: "root", storage, version: 1};
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER]
+      }
+    })
+  }
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
+
     <App />
+    </Provider>
   </React.StrictMode>
 );
