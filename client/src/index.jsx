6ee1cfa0ac7b8048d312f5adbc6ae2fd67d12130
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-05 01:47:32 -0400

    imported all react-persist and redux tools to save state into local state

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './assets/css/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import authReducer from "./state";
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import {
+  persistStore,
+  persistReducer,
+  Flush,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist"
+import storage from "redux-persist/lib/storage"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
