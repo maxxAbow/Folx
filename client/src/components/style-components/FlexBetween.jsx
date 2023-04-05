db0740fce5aac1208eb59a888c05aea3896b406e
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-05 11:30:29 -0400

    created flexBetween styling-components

--- client/src/components/style-components/FlexBetween.jsx
+++ client/src/components/style-components/FlexBetween.jsx
@@ -1,8 +1,8 @@
-// style-components are being used to re-use CSS styling as components
+// style-components are being used to re-use CSS styling properties as components inm different areas of our app
 import {Box} from '@mui/material';
 import {styled} from '@mui/system';
 
-function FlexBetween = styled(Box) ({
+const FlexBetween = styled(Box) ({
     display: "flex",
     justifyContent: "space-between",
     alignItems: "center"
