50e0b3b1bf184ffb4155aad591fc06e192aa21ca
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-06 19:11:47 -0400

    created widgewrapper styling component for base styling

---
+++ client/src/components/style-components/widgeWrap.jsx
@@ -0,0 +1,11 @@
+// style-components are being used to re-use CSS styling properties as components inm different areas of our app
+import {Box} from '@mui/material';
+import {styled} from '@mui/system';
+
+const widgeWrap = styled(Box)(({theme}) => ({
+    padding: "1.5rem 0.75rem 1.5rem",
+    backgroundColor: theme.palette.background.alt,
+    borderRadius: "0.75rem"
+}));
+
+export default widgeWrap;
