9b9aa938ae5807b316c61eb1991c13a2cb5c2bf8
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-06 19:27:03 -0400

    created profile image component

---
+++ client/src/components/ProfileImage.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+
+// Component for the user's profile image widget
+const ProfileImage = ({image, size="60px"}) => {
+    return (
+        <Box width={size} height={size}>
+
+        </Box>
+    )
+}
+
+export default ProfileImage;
--- client/src/components/style-components/widgeWrap.jsx
+++ client/src/components/style-components/widgeWrap.jsx
@@ -2,10 +2,10 @@
 import {Box} from '@mui/material';
 import {styled} from '@mui/system';
 
-const widgeWrap = styled(Box)(({theme}) => ({
+const WidgeWrap = styled(Box)(({theme}) => ({
     padding: "1.5rem 0.75rem 1.5rem",
     backgroundColor: theme.palette.background.alt,
     borderRadius: "0.75rem"
 }));
 
-export default widgeWrap;
+export default WidgeWrap;
