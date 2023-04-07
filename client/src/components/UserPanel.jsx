633865ab5f93113fcf6d447d0413f4f9b368f4df
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-07 10:44:04 -0400

    imported all tools

--- client/src/components/UserPanel.jsx
+++ client/src/components/UserPanel.jsx
@@ -1,4 +1,18 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import {
+    ManageAccountPutlined,
+    EditOutlined,
+    LocationOnOutlined,
+    WorkOutlineOutlined
+} from "@mui/icons-material";
+
+import {Box, Typography, Divider, useTheme } from "@mui/material";
+import ProfilePic from './ProfilePic';
+import FlexBetween from './style-components/FlexBetween';
+import WidgeWrap from './style-components/widgeWrap';
+import { useSelector } from 'react-redux';
 
 function UserPanel() {
   return (
