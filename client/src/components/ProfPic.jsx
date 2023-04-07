dfe38a008178c4ce1d7fab4e1f3168b0be8149a9
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-07 10:31:26 -0400

    renamed file to ProfilePic

--- client/src/components/ProfPic.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import {} from 'react-icons';
-
-const ProfPic = () => {
-  return <div>ProfPic</div>;
-};
-
-export default ProfPic;
--- client/src/components/ProfilePic.jsx
+++ client/src/components/ProfilePic.jsx
@@ -1,7 +1,7 @@
 import { Box } from "@mui/material";
 
 // Component for the user's profile image widget
-const ProfileImage = ({image, size="60px"}) => {
+const ProfilePic = ({image, size="60px"}) => {
     return (
         <Box width={size} height={size}>
             <img 
@@ -15,4 +15,4 @@ const ProfileImage = ({image, size="60px"}) => {
     )
 }
 
-export default ProfileImage;
+export default ProfilePic;
