31e6b3c7ee66fba6b5796d03f4cecc14e52850b3
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-08 23:45:19 -0400

    imported icons

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -1,4 +1,14 @@
-import React from 'react'
+import React from 'react';
+
+import {
+    AttachFileOutlined,
+    ImageOutlined,
+    MicOutlined,
+    EditOutlined,
+    GifBoxOutlined,
+    MoreHorizOutlined,
+    DeleteOutlined
+} from '@mui/icons-material';
 
 function CreatePost() {
   return (
