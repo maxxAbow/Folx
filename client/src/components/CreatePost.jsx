696477c24ec719fcdb0f60fda2936bd071838d86
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-09 00:16:07 -0400

    created variables to store state

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -27,7 +27,22 @@ import ProfilePic from './style-components/ProfilePic';
 import { useDispatch, useSelector } from 'react-redux';
 import {setPosts} from "state";
 
-function CreatePost() {
+function CreatePost({image}) {
+    const dispatch = useDispatch();
+    // State to represent if the user clicks the 'image button' to open up a place to drop an image
+    const [isImage, setIsImage] = useState(false);
+    // State to store the actuall image if the decide to dropit/upload it
+    const [uploadImage, isUploadImage] = useState(null)
+    // State to represent post content
+    const [post, setPost] = useState("");
+
+    const {palette} = useTheme();
+    const {_id} = useSelector((state) => state.user);
+    const token = useSelector((state) => state.token);
+    const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+    const mediumMain = palette.neutral.mediumMain;
+    const medium = palette.neutral.medium; 
+    
   return (
     <div>CreatePost</div>
   )
