adecca3c8e218ef27024279cab54951ac906dd48
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-09 11:29:02 -0400

    imported all components and tools to Timeline.jsx, and create useEffect

--- client/src/components/Timeline.jsx
+++ client/src/components/Timeline.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {setPosts} from "state";
+import Post from './Post';
+import api from 'utils/API';
 
-function Timeline() {
-  return (
-    <div>Timeline</div>
-  )
+function Timeline({userId, isProfile = false}) {
+    // const dispatch = useDispatch();
+    // const posts = useSelector((state) => state.posts);
+    // const token = useSelector((state) => state.token);
+
+    const getAllPosts = async () => {
+        const allPosts = await api.getPosts();
+    }
+    
+    const getUserPosts = async () => {
+        const userPost = await api.getPostById();
+    }
+
+    // If User is on a profile, it will onl retrieve made by that specific user, otherwise will retreive posts from everyone
+    useEffect(() => {
+        if(isProfile) {
+            getUserPosts();
+        } else {
+            getAllPosts();
+        }
+    }, [])
 }
 
 export default Timeline
