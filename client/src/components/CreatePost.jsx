3ded1d6123ecb2165c4a367e04b5040611f7660e
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-08 23:47:35 -0400

    imported components

--- client/src/components/CreatePost.jsx
+++ client/src/components/CreatePost.jsx
@@ -10,6 +10,17 @@ import {
     DeleteOutlined
 } from '@mui/icons-material';
 
+import { 
+    Box, 
+    Divider, 
+    Typography, 
+    InputBase, 
+    useTheme, 
+    Button, 
+    IconButton, 
+    useMediaQuery 
+} from '@mui/material';
+
 function CreatePost() {
   return (
     <div>CreatePost</div>
