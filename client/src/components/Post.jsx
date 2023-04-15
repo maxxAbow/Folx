dfa033dac51c0eb8a4130be16487f102dd853aa5
Daikeen Lockett <daikeen_lockett@yahoo.com>
2023-04-15 15:54:50 -0400

    completed/finished the like/unlike feature, updated routes to reflect this as well

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
 import {
   LocalPizzaOutlined, 
   NoMealsOutlined, 
@@ -11,57 +12,106 @@ import ProfilePic from './style-components/ProfilePic';
 import FlexBetween from './style-components/FlexBetween';
 import WidgeWrap from './style-components/WidgeWrap';
 import Friend from './Friend';
+import api from 'utils/API';
 
 
-const Post = (props) => {
+const Post = ({
+  user,
+  postId,
+  userId,
+  username,
+  description,
+  location,
+  userImage,
+  postImage,
+  likes,
+  date,
+  followers,
+  setFollowers,
+  following,
+  setFollowing
+}) => {
   const { palette } = useTheme();
   const main = palette.neutral.main;
   const primary = palette.primary.main;
 
-  const likes = props.likes.length
+  const [liked, setLiked] = useState(false);
+  const [loggedInUserId, setLoggedInUserId] = useState(null)
+  const [numLikes, setNumLikes] = useState(likes.length)
 
-  const [liked, setliked] = useState(false);
+  const navigate = useNavigate();
 
-  // if (!props.user) {
-  //   return null
-  // }
+  useEffect(() => {
+    const activeUserStorage = localStorage.getItem("activeUser");// Retrieves activerUsers data from local storage
+    const activeUser = JSON.parse(activeUserStorage); // parses the string into a JS object
+    
+    if (!activeUser || activeUser[1] !== true) {
+      navigate('/');
+    } else {
+      setLoggedInUserId({ loggedInUser: activeUser[0] });
+    }
+   
+    for (let i = 0; i < likes.length; i++) {
+      if (activeUser[0] === likes[i]){
+        setLiked(true);
+      }
+    }
+  }, []);
+
+  if (!user) {
+    return null
+  }
+
+  const like = async (postId, loggedInUserId) => {
+    await api.likePost(postId, loggedInUserId)
+    setNumLikes(numLikes + 1 );
+    setLiked(!liked)
+  };
+  
+  const unlike = async (postId) => {
+    await api.unlikePost(postId, loggedInUserId)
+    setNumLikes(numLikes - 1);
+    setLiked(!liked)
+  };
 
   return (
     <WidgeWrap margin="2rem 0">
       <Friend 
-      user={props.user}
-      friendId={props.userId}
-      username={props.username}
-      location={props.location}
-      image={props.userImage}
-      followers={props.followers} 
-      setFollowers={props.setFollowers}
-      following={props.following}
-      setFollowing={props.setFollowing}
+      user={user}
+      friendId={userId}
+      username={username}
+      location={location}
+      image={userImage}
+      followers={followers} 
+      setFollowers={setFollowers}
+      following={following}
+      setFollowing={setFollowing}
       />
       <Typography color={main} sx={{ mt: "1rem" }}>
-        {props.description}
+        {description}
       </Typography>
-      {props.postImage && (
+      {postImage && (
         <img
         width="100%"
         height="auto"
         alt="post"
         style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-        src={`../assets/images/post-images/${props.postImage}.jpg`}
+        src={`../assets/images/post-images/${postImage}.jpg`}
       />
       )}
       <FlexBetween marginTop="0.25rem">
         <FlexBetween gap="1rem">
           <FlexBetween gap="0.3rem">
-            <IconButton onClick={() => setliked(!liked)}>
-            {liked ? (
-                <LocalPizzaOutlined sx={{ color: primary }} />
-              ) : (
-                <LocalPizzaOutlined />
+            {!liked ? (
+              <IconButton onClick={() => like(postId, loggedInUserId)}>
+              <LocalPizzaOutlined />
+              </IconButton>
+            ) : (
+              <IconButton onClick={() => unlike(postId, loggedInUserId)}>
+              <LocalPizzaOutlined sx={{ color: primary }} />
+              </IconButton>
             )}
-            </IconButton>
-            <Typography>{likes}</Typography>
+            <Typography>{numLikes}</Typography>
           </FlexBetween>
           <FlexBetween gap="0.3rem">
             <IconButton>
