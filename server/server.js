4e64cba2607a40c40ebbdb6565deb8bdce591dd3
David Mackey <davidmackey92@gmail.com>
2023-04-16 00:54:53 -0400

    testing deployment

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static(path.join(__dirname + '/public')));
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
