35805d3e7e881b52ac5a65b72852c9477a186a9f
David Mackey <davidmackey92@gmail.com>
2023-04-16 00:52:24 -0400

    testing deployment

--- server/server.js
+++ server/server.js
@@ -10,7 +10,14 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname + '/public')));
+// app.use(express.static(path.join(__dirname + '/public')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const sess = {
   secret: 'Folx secret',
